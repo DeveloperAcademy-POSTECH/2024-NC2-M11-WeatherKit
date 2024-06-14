43fd38af2df33460bb372c7a783b6b9bb01b3099
TakeMos <kangh0423@gmail.com>
2024-06-14 10:23:58 +0900

    [Setting] 프로젝트 파일 업로드

---
+++ KwangKwang/KwangKwang/KwangKwangApp.swift
@@ -0,0 +1,17 @@
+//
+//  KwangKwangApp.swift
+//  KwangKwang
+//
+//  Created by kim kanghyeok on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct KwangKwangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
