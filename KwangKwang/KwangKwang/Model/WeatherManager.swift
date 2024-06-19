e8948b2b4cc15b17e3d9d8cd85fdbcf032b39daf
TakeMos <kangh0423@gmail.com>
2024-06-20 05:07:22 +0900

    Revert "[Fix] #1 - 이미지 파일 수정 및 weather 서비스 불러오는 부분 수정"
    This reverts commit b797ca8d172f0a81ff894585cfc9e684916f4db8.

--- KwangKwang/KwangKwang/Model/WeatherManager.swift
+++ KwangKwang/KwangKwang/Model/WeatherManager.swift
@@ -9,10 +9,18 @@ class WeatherManager: ObservableObject {
     
     func getWeather(lat: Double, long: Double) async {
         do {
-            weather = try await weatherService.weather(for: .init(latitude: lat, longitude: long))
+            weather = try await Task.detached(priority: .userInitiated) { [weak self] in
+                return try await self?.weatherService.weather(for: .init(latitude: lat, longitude: long))
+            }.value
+            
         } catch {
             print("Failed to get weather data. \(error)")
         }
+    }
+    var icon: String {
+        guard let iconName = weather?.currentWeather.symbolName else { return "--" }
+        
+        return iconName
     }
     
     var currentWeather: CurrentWeather? {
@@ -42,6 +50,20 @@ class WeatherManager: ObservableObject {
             print("hourly weather Error")
             return nil
         }
+    }
+    
+    var temperature: String {
+        guard let temp = weather?.currentWeather.temperature else { return "--" }
+        let convert = temp.converted(to: .celsius).value
+        
+        return String(Int(convert)) + "°C"
+    }
+    
+    var humidity: String {
+        guard let humidity = weather?.currentWeather.humidity else { return "--" }
+        let computedHumidity = humidity * 100
+        
+        return String(Int(computedHumidity)) + "%"
     }
     
     var date: String {
